e035d14687013a132df4a39f4e08b7cd28989585
RakibRahman <md.rr.talukder@gmail.com>
2021-06-11 22:40:23 +0600

    comment updated

--- js/questions.js
+++ js/questions.js
@@ -1,9 +1,11 @@
+/*jshint esversion: 6 */
+
 export default function Question(question, choices, answer) {
   this.question = question;
   this.choices = choices;
   this.answer = answer;
 }
-//Check given answer is valid or not
+//Check selected answer is valid or not
 
 Question.prototype.isCorrect = function (guessKey) {
   return guessKey === this.answer;
